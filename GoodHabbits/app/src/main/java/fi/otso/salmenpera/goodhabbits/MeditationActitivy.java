3b33a5fa6c38ee21e9637b62a1efa8d2a19cc510
sotasorsa2 <otso.salmenpera@metropolia.fi>
2022-04-22 11:32:14 +0300

    add meditationActivity

---
+++ GoodHabbits/app/src/main/java/fi/otso/salmenpera/goodhabbits/MeditationActitivy.java
@@ -0,0 +1,14 @@
+package fi.otso.salmenpera.goodhabbits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeditationActitivy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditation_actitivy);
+    }
+}
